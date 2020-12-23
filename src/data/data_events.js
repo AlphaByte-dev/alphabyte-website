2ed46eb5a680c6595c6d9f34a2492a4aa5ada177
Pawan Kolhe <contact@pawankolhe.com>
2020-12-24 01:38:35 +0530

    events page

---
+++ src/data/data_events.js
@@ -0,0 +1,17 @@
+const events_data = [
+  {
+    name: "Hacktoberfest - Open Source, Git & GitHub",
+    description: "Get started with Open Source and make your very first contribution in Hacktoberfest. We will cover the basic of Git and how to use GitHub to your advantage.",
+    image: "./assets/images/events/Hacktoberfest2020.png",
+  },
+  {
+    name: "Hacktoberfest - HTML & CSS",
+    description: "Everybody desires to have their own website. Get hands-on experience building a website from scratch using HTML and CSS, the building blocks of the web.",
+    image: "./assets/images/events/Hacktoberfest2020.png",
+  },
+  {
+    name: "Intro to Python",
+    description: "Guest speaker, Harshit Shukla, will teach Python with a hands-on session.",
+    image: "./assets/images/events/Python-Workshop.png",
+  },
+];
--- src/js/events.js
+++ src/js/events.js
@@ -1,1 +1,29 @@
-const about = 'about!';
+(() => {
+  const eventsHTMLElement = document.querySelector('#past-events');
+
+  const fragment = document.createDocumentFragment();
+  // events_data.sort((a, b) => (a.fullname < b.fullname) ? -1 : (a.fullname > b.fullname) ? 1 : 0);
+  events_data.forEach(event => {
+    const div = document.createElement('div');
+    div.className = 'event';
+    div.innerHTML = `
+    <a href="${event.image}" class="event__image">
+      <img src="${event.image}" alt="event image">
+    </a>
+    <div class="event__info">
+      <div class="event__name">${event.name}</div>
+      <div class="event__description">${event.description}</div>
+    </div>
+    `;
+    fragment.appendChild(div);
+  });
+
+  eventsHTMLElement.appendChild(fragment);
+
+  new SimpleLightbox('.event__image', {
+    loop: false,
+    nav: false,
+    showCounter: false,
+    uniqueImages: false,
+  });
+})();
